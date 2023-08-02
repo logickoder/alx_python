2e7ab30f3c2d41aae94d07c72597436cbc1e68b0
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2023-08-02 16:07:14 +0100

    4. Fibonacci Sequence Function

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci_sequence(n):
+    """Return a list of the first n numbers of the Fibonacci sequence."""
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib = [0, 1]
+        for i in range(2, n):
+            fib.append(fib[i - 1] + fib[i - 2])
+        return fib
