673a0d96387a87feda04a6025cf04145166839f4
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2023-08-12 11:42:38 +0100

    0. Can you C me now?

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,7 @@
+def no_c(my_string: str) -> str:
+    """Removes all characters 'c' and 'C' from a string."""
+    new_string = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            new_string += char
+    return new_string
