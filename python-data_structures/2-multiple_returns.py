eb85d0d0bff12e6000f22a26f25dbce483482e00
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2023-08-12 11:45:14 +0100

    2. More returns!

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,6 @@
+def multiple_returns(sentence):
+    """Returns a tuple with the length of a string and its first character."""
+    if sentence:
+        return (len(sentence), sentence[0])
+    else:
+        return (0, None)
