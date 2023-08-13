bbe54980654c6a86fdf3d9e395a94bd2576cce7c
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2023-08-13 07:01:57 +0100

    0. Square with size

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """__init__ method
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
