b1f74ca5670621d45c2db72b1f63b974b5e52e95
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2023-08-17 15:18:09 +0100

    3. Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,5 +6,13 @@ BaseGeometry class
 class BaseGeometry:
     """BaseGeometry class"""
 
-    def __init__(self) -> None:
-        pass
+    def area(self):
+        """area method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer_validator method"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
