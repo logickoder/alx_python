79be158fa8d35abe0a453b33a7f520a3579f57d2
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2023-08-17 15:29:20 +0100

    3. Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,13 +3,10 @@ BaseGeometry class
 """
 
 
-from collections.abc import Iterable
-
-
 class BaseGeometry:
     """BaseGeometry class"""
 
-    def __dir__(self) -> Iterable[str]:
+    def __dir__(self):
         """Returns the list of available attributes and methods of an object"""
         """Remove __init_subclass__ from the list"""
         return [item for item in dir(self) if item != "__init_subclass__"]
